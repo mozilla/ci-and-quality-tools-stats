54558f6b99c51b0366e6df41f2fc8bb56e979bf8
Suhaib Mujahid <smujahid@mozilla.com>
2023-01-17 18:52:29 -0500

    Add pending needinfo stage

--- scripts/stages.py
+++ scripts/stages.py
@@ -2,8 +2,11 @@
 # Generate events for bug from the Bugzilla
 from enum import Enum, auto
 from bugbug import db, bugzilla
+import re
 
-OLDEST_BUG = "2021-01-01"
+OLDEST_BUG = "2020-01-01"
+
+NI_PAT = re.compile(r"needinfo\?\((.*?)\)")
 
 
 class Stage(Enum):
@@ -11,6 +14,8 @@ class Stage(Enum):
     NO_COMPONENT = auto()
     UNCONFIRMED = auto()
     CONFIRMED = auto()
+    PENDING_NEEDINFO = auto()
+    REMOVE_NEEDINFO = auto()
     TRIAGED = auto()
     ASSIGNED = auto()
     IN_REVIEW = auto()
@@ -70,6 +75,15 @@ for bug in bugzilla.get_bugs():
     first_patch_at = None
     bug_events = []
 
+    current_needinfos = []
+    needinfo_events = [
+        {
+            "time": bug["creation_time"],
+            "add": [],
+            "remove": [],
+        }
+    ]
+
     # Get stage changing events from the bug history
     for event in bug["history"]:
         for change in event["changes"]:
@@ -100,6 +114,55 @@ for bug in bugzilla.get_bugs():
                     is_moved_to_component_after_open = True
                     # If the bug was confirmed at this time, this even will be filtered out
                     bug_events.append((event["when"], Stage.UNCONFIRMED))
+            elif change["field_name"] == "flagtypes.name":
+                removed_needinfos = NI_PAT.findall(change["removed"])
+                added_needinfos = NI_PAT.findall(change["added"])
+                needinfo_events.append(
+                    {
+                        "time": event["when"],
+                        "add": added_needinfos,
+                        "remove": removed_needinfos,
+                    }
+                )
+                for email in removed_needinfos:
+                    try:
+                        current_needinfos.remove(email)
+                    except ValueError:
+                        # This means that the needinfo was added at the creation time
+                        needinfo_events[0]["add"].append(email)
+
+                current_needinfos.extend(added_needinfos)
+
+    # Add needinfos that was added at the creation time and still pending
+    needinfo_events[0]["add"].extend(
+        flag["requestee"]
+        for flag in bug["flags"]
+        if flag["name"] == "needinfo" and flag["requestee"] not in current_needinfos
+    )
+
+    # Convert needinfo events to stage events
+    current_needinfos = []  # reset the list
+    for event in needinfo_events:
+        was_pending = len(current_needinfos) > 0
+        for email in event["remove"]:
+            current_needinfos.remove(email)
+        current_needinfos.extend(event["add"])
+
+        if was_pending and len(current_needinfos) == 0:
+            bug_events.append(
+                (
+                    event["time"],
+                    Stage.REMOVE_NEEDINFO,
+                )
+            )
+
+        elif not was_pending and len(current_needinfos) > 0:
+            bug_events.append(
+                (
+                    event["time"],
+                    Stage.PENDING_NEEDINFO,
+                )
+            )
 
     # Get the date for the first patch
     for attachment in bug["attachments"]:
@@ -136,7 +199,19 @@ for bug in bugzilla.get_bugs():
     bug_events.sort()
     last_stage = Stage.NOTHING
     for when, stage in bug_events:
-        if stage > last_stage:
+        if stage is Stage.REMOVE_NEEDINFO:
+            if last_stage is not Stage.PENDING_NEEDINFO:
+                continue
+
+            for event in reversed(events):
+                if event[1] not in (Stage.PENDING_NEEDINFO, Stage.REMOVE_NEEDINFO):
+                    stage = event[1]
+                    break
+                if event[2] is Stage.NOTHING:
+                    raise Exception("No stage found for REMOVE_NEEDINFO")
+
+        if stage > last_stage or last_stage is Stage.PENDING_NEEDINFO:
+            # Ignore stages that go backwards
             events.append((when, stage, last_stage))
             last_stage = stage
 
@@ -147,7 +222,6 @@ events.sort()
 
 # %%
 # Aggregate te events by day
-CHART_STARTS_AFTER = "2022-01-01"
 
 day_status = current_status.copy()
 last_day = None
@@ -155,7 +229,7 @@ last_day = None
 status_by_day = {stage: [] for stage in day_status}
 dates = []
 
-last_day = CHART_STARTS_AFTER
+last_day = ""
 for when, stage, last_stage in events:
     day = when[:10]
     if day > last_day:
@@ -178,18 +252,27 @@ plt.style.use("seaborn-v0_8-whitegrid")
 
 
 labels = sorted(
-    [stage for stage in status_by_day if stage != Stage.NOTHING], reverse=True
+    [
+        stage
+        for stage in status_by_day
+        if stage not in (Stage.NOTHING, stage.REMOVE_NEEDINFO)
+    ],
+    reverse=True,
 )
 x = np.array(dates).astype(np.datetime64)
 y = np.vstack(status_by_day[stage] for stage in labels)
 
+first_date = np.datetime64("2022-06-01")
+first_date_idx = np.where(x == first_date)[0][0]
+min_y_value = y[0][first_date_idx] - 1000
+
 fig, ax = plt.subplots()
 ax.stackplot(x, y, labels=labels, alpha=0.8)
-ax.legend(bbox_to_anchor=(1.32, 0.5), loc="center right")
+ax.legend(bbox_to_anchor=(1.35, 0.5), loc="center right")
 ax.set(
-    ylim=(20000, None),
-    xlim=(x[0], x[-1]),
-    title=f"Workflow for defect bugs from bugbug (created since {OLDEST_BUG})",
+    ylim=(min_y_value, None),
+    xlim=(first_date, x[-1]),
+    title=f"Workflow for defect bugs created since {OLDEST_BUG}",
     ylabel="Number of Bugs",
 )
 ax.tick_params(axis="x", labelrotation=40)
