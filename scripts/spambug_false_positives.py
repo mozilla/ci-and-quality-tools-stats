0cf0f00de94e818993ddb7eb06002d7faa4794de
Benjamin Mah <benjaminmah2004@gmail.com>
2024-07-09 18:34:18 -0400

    Updated script to find bugs that have reverted their resolution

--- scripts/spambug_false_positives.py
+++ scripts/spambug_false_positives.py
@@ -1,22 +1,20 @@
 from bugbug import db, bugzilla
 
-# Download the Bugzilla database
 db.download(bugzilla.BUGS_DB)
 
-# Function to process bug history
-def process_bug_history():
+def search_false_positives():
     counter = 0
     for bug in bugzilla.get_bugs():
         for transaction in bug['history']:
-            if transaction['who'] == 'release-mgmt-account-bot@mozilla.tld':
+            if transaction['who'] == 'release-mgmt-account-bot@mozilla.tld' and bug['resolution'] != 'INVALID':
                 for change in transaction['changes']:
-                    if change['field_name'] == 'resolution' and change['added'] == 'INVALID':
-                        print(f"Bug ID: {bug['id']}")
-                        print(f"Transaction: {transaction}")
+                    if change['field_name'] == 'resolution' and change['added'] == 'INVALID' and bug['resolution'] != 'INVALID':
+                        print(f"Bug ID: {bug['id']}")   
                         counter += 1
-    
+                        break
     return counter
+
 if __name__ == "__main__":
     db.download(bugzilla.BUGS_DB)
-    counter = process_bug_history()
+    counter = search_false_positives()
     print(f"Number of bugs marked as false positives: {counter}")
