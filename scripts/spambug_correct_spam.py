6d1354e31399def24bf86daa2b1b8e3e3e02cdad
Benjamin Mah <benjaminmah2004@gmail.com>
2024-07-12 11:01:33 -0400

    Updated scripts to reduce nesting

--- scripts/spambug_correct_spam.py
+++ scripts/spambug_correct_spam.py
@@ -1,23 +1,28 @@
 from bugbug import db, bugzilla
 
+def contains_spam_comment(comments, comment_text):
+    for comment in comments:
+        if comment_text in comment['text']:
+            return True
+    return False
+
 def search_correct_spam():
     comment_text = """If you think the bot is wrong, please reopen the bug and move it back to its prior component."""
-
     counter = 0
+
     for bug in bugzilla.get_bugs():
-        if bug['resolution'] == 'INVALID':
-            for comment in bug['comments']:
-                if comment_text in comment['text']:
-                    print(f"Bug ID: {bug['id']}")
-                    counter += 1
+        if bug['resolution'] == 'INVALID' and contains_spam_comment(bug['comments'], comment_text):
+            print(f"Bug ID: {bug['id']}")
+            counter += 1
 
     return counter
+
 if __name__ == "__main__":
-    # db.register(
-    #     bugzilla.BUGS_DB,
-    #     "https://community-tc.services.mozilla.com/api/index/v1/task/project.bugbug.data_bugs.latest/artifacts/public/bugs.json.zst",
-    #     10
-    # )
-    # db.download(bugzilla.BUGS_DB)
+    db.register(
+        bugzilla.BUGS_DB,
+        "https://community-tc.services.mozilla.com/api/index/v1/task/project.bugbug.data_bugs.latest/artifacts/public/bugs.json.zst",
+        10
+    )
+    db.download(bugzilla.BUGS_DB)
     counter = search_correct_spam()
     print(f"Number of bugs marked as spam: {counter}")
--- scripts/spambug_false_positives.py
+++ scripts/spambug_false_positives.py
@@ -1,15 +1,24 @@
 from bugbug import db, bugzilla
 
+def is_false_positive(transaction, bug):
+    if transaction['who'] != 'release-mgmt-account-bot@mozilla.tld' or bug['resolution'] == 'INVALID':
+        return False
+    
+    for change in transaction['changes']:
+        if change['field_name'] == 'resolution' and change['added'] == 'INVALID':
+            return True
+    
+    return False
 
 def search_false_positives():
     counter = 0
     for bug in bugzilla.get_bugs():
         for transaction in bug['history']:
-            if transaction['who'] == 'release-mgmt-account-bot@mozilla.tld' and bug['resolution'] != 'INVALID':
-                for change in transaction['changes']:
-                    if change['field_name'] == 'resolution' and change['added'] == 'INVALID':
-                        print(f"Bug ID: {bug['id']}")   
-                        counter += 1
+            if is_false_positive(transaction, bug):
+                print(f"Bug ID: {bug['id']}")
+                counter += 1
+                break 
+
     return counter
 
 if __name__ == "__main__":
