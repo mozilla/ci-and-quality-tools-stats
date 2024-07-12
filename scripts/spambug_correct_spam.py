a5e9f64873ad1173d9f33126a23e0b895a875a50
Benjamin Mah <benjaminmah2004@gmail.com>
2024-07-12 09:52:37 -0400

    Uncommented script

--- scripts/spambug_correct_spam.py
+++ scripts/spambug_correct_spam.py
@@ -1,26 +1,15 @@
 from bugbug import db, bugzilla
 
-
-# def search_correct_spam():
-#     counter = 0
-#     for bug in bugzilla.get_bugs():
-#         for transaction in bug['history']:
-#             if transaction['who'] == 'release-mgmt-account-bot@mozilla.tld' and bug['resolution'] == 'INVALID':
-#                 for change in transaction['changes']:
-#                     if change['field_name'] == 'resolution' and change['added'] == 'INVALID':
-#                         print(f"Bug ID: {bug['id']}")   
-#                         counter += 1
-
-#     return counter
 def search_correct_spam():
     comment_text = """If you think the bot is wrong, please reopen the bug and move it back to its prior component."""
 
     counter = 0
     for bug in bugzilla.get_bugs():
-        for comment in bug['comments']:
-            if comment_text in comment['text']:
-                print(f"Bug ID: {bug['id']}")
-                counter += 1
+        if bug['resolution'] == 'INVALID':
+            for comment in bug['comments']:
+                if comment_text in comment['text']:
+                    print(f"Bug ID: {bug['id']}")
+                    counter += 1
 
     return counter
 if __name__ == "__main__":
--- scripts/spambug_false_positives.py
+++ scripts/spambug_false_positives.py
@@ -7,18 +7,17 @@ def search_false_positives():
         for transaction in bug['history']:
             if transaction['who'] == 'release-mgmt-account-bot@mozilla.tld' and bug['resolution'] != 'INVALID':
                 for change in transaction['changes']:
-                    if change['field_name'] == 'resolution' and change['added'] == 'INVALID' and bug['resolution'] != 'INVALID':
+                    if change['field_name'] == 'resolution' and change['added'] == 'INVALID':
                         print(f"Bug ID: {bug['id']}")   
                         counter += 1
-                        break
     return counter
 
 if __name__ == "__main__":
-    # db.register(
-    #     bugzilla.BUGS_DB,
-    #     "https://community-tc.services.mozilla.com/api/index/v1/task/project.bugbug.data_bugs.latest/artifacts/public/bugs.json.zst",
-    #     10
-    # )
-    # db.download(bugzilla.BUGS_DB)
+    db.register(
+        bugzilla.BUGS_DB,
+        "https://community-tc.services.mozilla.com/api/index/v1/task/project.bugbug.data_bugs.latest/artifacts/public/bugs.json.zst",
+        10
+    )
+    db.download(bugzilla.BUGS_DB)
     counter = search_false_positives()
     print(f"Number of bugs marked as false positives: {counter}")
